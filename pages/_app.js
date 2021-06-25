7cd6435657624d944fc5455e8906c6d2597e9fd5
biplob <biplobhasan547@gmail.com>
2021-06-26 03:53:03 +0600

    backdrop added as loading effect making

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,21 @@ import NProgress from "nprogress"; //nprogress module
 import "nprogress/nprogress.css"; //styles of nprogress
 
 import { useEffect } from "react";
+// backdrop create start
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import ReactLoading from "react-loading";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: "#fff",
+    },
+}));
+// backdrop end
+
+// for Nprogress
 NProgress.configure({
     minimum: 0.3,
     easing: "ease",
@@ -30,6 +44,7 @@ Router.onRouteChangeComplete = () => {
 Router.onRouteChangeError = () => {
     NProgress.done();
 };
+// end
 
 // crisp
 import dynamic from "next/dynamic";
@@ -37,10 +52,20 @@ import dynamic from "next/dynamic";
 const CrispWithNoSSR = dynamic(() => import("../components/crisp"), {
     ssr: false,
 });
+// end
 
 export default function MyApp(props) {
     const { Component, pageProps } = props;
     const store = useStore(pageProps.initialReduxState);
+
+    // for backdrop
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    Router.events.on("routeChangeStart", () => setOpen(true));
+    Router.events.on("routeChangeComplete", () => setOpen(false));
+    Router.events.on("routeChangeError", () => setOpen(false));
+    // backdrop end
 
     useEffect(() => {
         // Remove the server-side injected CSS.
@@ -58,6 +83,10 @@ export default function MyApp(props) {
                     content="minimum-scale=1, initial-scale=1, width=device-width"
                 />
             </Head>
+            {/* backdrop added */}
+            <Backdrop className={classes.backdrop} open={open}>
+                <ReactLoading type="spinningBubbles" />
+            </Backdrop>
             <ThemeProvider theme={theme}>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
